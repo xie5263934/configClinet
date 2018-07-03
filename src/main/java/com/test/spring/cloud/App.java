0bcf644e2a81368804f3173e44a55016576966c2
jinrun.xie <jinrun.xie@56qq.com>
2018-07-03 23:38:46 +0800

    首次提交

---
+++ src/main/java/com/test/spring/cloud/App.java
@@ -0,0 +1,27 @@
+package com.test.spring.cloud;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sheldon on 2018/7/3.
+ */
+@SpringBootApplication
+@RestController
+public class App {
+    @Value("${first.age}")
+    private String name;
+
+    public static void main(String [] args){
+        SpringApplication.run(App.class, args);
+    }
+
+    @RequestMapping(value = "/")
+    public String getName(){
+        return this.name;
+    }
+
+}
