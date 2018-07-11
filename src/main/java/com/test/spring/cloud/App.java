a61a94f581bc880e79a9b65234e81ca06628e49c
sheldonxie <jinrun.xie@56qq.com>
2018-07-11 18:12:40 +0800

    提交

--- src/main/java/com/test/spring/cloud/App.java
+++ src/main/java/com/test/spring/cloud/App.java
@@ -3,6 +3,7 @@ package com.test.spring.cloud;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @RefreshScope
+@EnableDiscoveryClient
 public class App {
     @Value("${first.age}")
     private String name;
